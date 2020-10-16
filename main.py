bfda299925dcdacb617fbff9c4aec2442e6bce07
Pierre-Antoine Chéron <pierre-antoine.cheron@etu.umontpellier.fr>
2020-10-16 16:46:29 +0200

    a first, very bad attempt

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 # %%
 import numpy as np
 
-prevLayerSize = 30
-layerSize = 20
+def createLayer(prevLayerSize, layerSize):
+    weights = np.random.rand(prevLayerSize,layerSize)
+    biases = np.random.rand(prevLayerSize)
+    values = np.ones(layerSize)
+    return [weights, biases, values]
 
-prevLayerValues = np.random.rand(prevLayerSize)
-weights = np.random.rand(prevLayerSize,layerSize)
-biases = np.random.rand(prevLayerSize)
+
+def updateLayer(prevLayer, layer):
+    layer[2] = prevLayer[2]*layer[0]+layer[1]
+
+def createNetwork(layersSizes):
+    layers = [createLayer(i,i+1) for i in range(len(layersSizes)-1)]
+    return layers
+
+def updateNetwork(inputs, network):
+    network[0][2] = inputs
+    for i in range(len(network)-1):
+        updateLayer(network[i],network[i+1])
 
 # %%
+net = createNetwork([2,2,2])
+for layer in range(len(net)):
+    print(net[layer])
+    print("\n")
+# %%
+updateNetwork([1,2], net)
