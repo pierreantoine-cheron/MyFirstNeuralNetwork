b28e03ea9b6a3fcbdf288ad4cbadd28e1e0184d6
Pierre-Antoine Chéron <pierre-antoine.cheron@etu.umontpellier.fr>
2020-10-16 15:30:26 +0200

    layer structure

---
+++ main.py
@@ -0,0 +1,11 @@
+# %%
+import numpy as np
+
+prevLayerSize = 30
+layerSize = 20
+
+prevLayerValues = np.random.rand(prevLayerSize)
+weights = np.random.rand(prevLayerSize,layerSize)
+biases = np.random.rand(prevLayerSize)
+
+# %%
